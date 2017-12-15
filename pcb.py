da02a78b2890d76139f01090e64fd88b018b88b6
Michael Kirsch <michaelkirsch@kintaro.co>
2017-12-15 12:20:34 +0100

    First working python 3 port

--- pcb.py
+++ pcb.py
@@ -13,7 +13,7 @@ import subprocess
 from configparser import SafeConfigParser
 from enum import Enum
 
-class pcb_components(Enum):
+class pcb_components():
     LED = 7
     FAN = 8
     RESET = 3
@@ -22,7 +22,7 @@ class pcb_components(Enum):
 
 class path():
     kintaro_folder = "/opt/kintaro/"
-    start_folder = kintaro_folder + "start/"
+    start_folder = "start/"
     intro_video = kintaro_folder + start_folder + "intro.mp4"
     config_file = kintaro_folder + start_folder + "kintaro.config"
     temp_command = 'vcgencmd measure_temp'
@@ -36,25 +36,24 @@ class vars():
     counter_time = 0.01
 
 GPIO.setmode(GPIO.BOARD) #Use the same layout as the pins
-GPIO.setup(pcb_components.LED.value, GPIO.OUT) #LED Output
-GPIO.setup(pcb_components.FAN.value, GPIO.OUT) #FAN Output
-GPIO.setup(pcb_components.POWER.value, GPIO.IN)  #set pin as input
-GPIO.setup(pcb_components.RESET.value, GPIO.IN, pull_up_down=GPIO.PUD_UP) #set pin as input and switch on internal pull up resistor
-GPIO.setup(pcb_components.CHECK_PCB.value, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(pcb_components.LED, GPIO.OUT) #LED Output
+GPIO.setup(pcb_components.FAN, GPIO.OUT) #FAN Output
+GPIO.setup(pcb_components.POWER, GPIO.IN)  #set pin as input
+GPIO.setup(pcb_components.RESET, GPIO.IN, pull_up_down=GPIO.PUD_UP) #set pin as input and switch on internal pull up resistor
+GPIO.setup(pcb_components.CHECK_PCB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def temp(): #returns the gpu temoperature
     res = os.popen(path.temp_command).readline()
-    return (res.replace("temp=", "").replace("C\n", ""))
+    return float((res.replace("temp=", "").replace("'C\n", "")))
 
 class led:  #class to control the led
-
-    def toggle(self,status):  #toggle the led on of off
+    def toggle(status):  #toggle the led on of off
         if status == 0:       #the led is inverted
-            GPIO.output(LED, GPIO.LOW)
+            GPIO.output(pcb_components.LED, GPIO.LOW)
         if status == 1:
-            GPIO.output(LED, GPIO.HIGH)
+            GPIO.output(pcb_components.LED, GPIO.HIGH)
 
-    def blink(self,amount,interval): #blink the led
+    def blink(amount,interval): #blink the led
         for x in range(amount):
             toggle(1)
             time.sleep(interval)
@@ -68,9 +67,9 @@ def return_config_bool(searchterm):
 
 def fan(status):  #switch the fan on or off
     if status == 1:
-        GPIO.output(FAN, GPIO.HIGH)
+        GPIO.output(pcb_components.FAN, GPIO.HIGH)
     if status == 0:
-        GPIO.output(FAN, GPIO.LOW)
+        GPIO.output(pcb_components.FAN, GPIO.LOW)
 
 def fancontrol(hysteresis,starttemp):  #read the temp and have a buildin hysteresis
     if temp() > starttemp:
@@ -79,10 +78,10 @@ def fancontrol(hysteresis,starttemp):  #read the temp and have a buildin hystere
         fan(0)
 
 if return_config_bool("video"):
-    os.system("omxplayer" + path.intro_video + "&") #start the bootvideo on start
+    os.system("omxplayer " + path.intro_video + " &") #start the bootvideo on start
 
 def toggle(toggle_this):  #change one of the values in the config file
-    parser = SafeConfigParser()
+    parser = ConfigParser()
     parser.read(path.config_file)
     if return_config_bool(toggle_this):
         parser.set('Boot', toggle_this, "False")
@@ -93,17 +92,17 @@ def toggle(toggle_this):  #change one of the values in the config file
         parser.write(configfile)
 
 def check_powerbutton():
-    if (GPIO.input(POWER) == GPIO.HIGH) and GPIO.input(CHECK_PCB) == GPIO.LOW:  # shutdown funktion if the powerswitch is toggled
+    if (GPIO.input(pcb_components.POWER) == GPIO.HIGH) and GPIO.input(pcb_components.CHECK_PCB) == GPIO.LOW:  # shutdown funktion if the powerswitch is toggled
         os.system("killall emulationstation")
         led.toggle(0)
         fan(0)
         os.system("sudo shutdown -h now")
 
 def check_resetbutton():
-    if (GPIO.input(RESET) == GPIO.LOW):  # reset function
+    if (GPIO.input(pcb_components.RESET) == GPIO.LOW):  # reset function
         reset_counter = 0  # counter for the time funktion
         time.sleep(vars.debounce_time)  # debounce time
-        while (GPIO.input(RESET) == GPIO.LOW):  # while the button is hold the counter counts up
+        while (GPIO.input(pcb_components.RESET) == GPIO.LOW):  # while the button is hold the counter counts up
             reset_counter = reset_counter + 1
             time.sleep(vars.counter_time)
         if reset_counter > vars.reset_hold_short:  # check if its hold more that one second
@@ -118,7 +117,7 @@ def check_resetbutton():
             os.system("sudo reboot")
 
 while True:
-    if return_config_bool("pcb") and GPIO.input(CHECK_PCB)==GPIO.LOW: #check if there is an pcb and if there is
+    if return_config_bool("pcb") and GPIO.input(pcb_components.CHECK_PCB)==GPIO.LOW: #check if there is an pcb and if there is
         led.toggle(1)
         if return_config_bool("fan"): #check if the fan is activated in the config
             fancontrol(vars.fan_hysteresis , vars.fan_starttemp) # fan starts at 60 degrees and has a 5 degree hysteresis
