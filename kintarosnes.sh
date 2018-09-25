e04a9a97a99edc2300639fb5bb1d2665772f6da4
michaelkirsch <michael_kirsch@t-online.de>
2018-09-25 22:11:15 +0200

    add ed Features

--- kintarosnes.sh
+++ kintarosnes.sh
@@ -1,41 +1,94 @@
 #!/usr/bin/env bash
 
+rp_module_id="kintaro"
+rp_module_desc="kintaro Driver"
+rp_module_section="config"
 
-
-rp_module_id="kintarosnes"
-rp_module_desc="Snes PCB Driver"
-rp_module_section="driver"
-rp_module_flags="noinstclean"
-rp_module_help="Keep in mind that ..."
-
-function update_kintaro() {
-    # install from there to system folders
-    sudo apt-get update
-    sudo apt-get upgrade kintarosnes
-}
-
-function gui_kintaro() {
+function pwm_menu() {
     local cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option." 22 86 16)
     local options=(
-        1 "Enable PowerBlock driver"
-        2 "Disable PowerBlock driver"
+        1 "Start at 40 deg. 100% at 60 deg."
+        2 "Start at 50 deg. 100% at 70 deg."
 
     )
     local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
     if [[ -n "$choice" ]]; then
         case "$choice" in
             1)
-                make -C "$md_inst/build" installservice
-                printMsgs "dialog" "Enabled PowerBlock driver."
+
+                printMsgs "dialog" "Enabled ControlBlock driver."
                 ;;
             2)
-                make -C "$md_inst/build" uninstallservice
-                printMsgs "dialog" "Disabled PowerBlock driver."
+                #change to normal fan here
+                printMsgs "Using default I/O-Fan controll !"
                 ;;
         esac
     fi
 }
 
-function remove_kintaro() {
-    sudo ./opt/kintaro/remove.sh
+
+function change_fan() {
+    local cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option." 22 86 16)
+    local options=(
+        1 "Activate and setup PWM-Fan"
+        2 "Use default fan mode"
+
+    )
+    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+    if [[ -n "$choice" ]]; then
+        case "$choice" in
+            1)
+                pwm_menu
+                printMsgs "dialog" "Enabled ControlBlock driver."
+                ;;
+            2)
+                #change to normal fan here
+                printMsgs "Using default I/O-Fan controll !"
+                ;;
+        esac
+    fi
 }
+
+function gui_kintaro() {
+   addAutoConf "8bitdo_hack" 0
+
+    while true; do
+        local connect_mode="$(_get_connect_mode)"
+
+        local cmd=(dialog --backtitle "$__backtitle" --menu "Configure Bluetooth Devices" 22 76 16)
+        local options=(
+            R "Fan-Setup"
+            X "Update the package"
+            D "Play custom video"
+            U "LED"
+            C "Remove Kintaro Driver"
+        )
+
+        local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        if [[ -n "$choice" ]]; then
+            # temporarily restore Bluetooth stack (if needed)
+            service sixad status >/dev/null && sixad -r
+            case "$choice" in
+                R)
+                    change_fan
+                    ;;
+                X)
+                    remove_device_bluetooth
+                    ;;
+                D)
+                    display_active_and_registered_bluetooth
+                    ;;
+                U)
+                    udev_bluetooth
+                    ;;
+                C)
+                    connect_bluetooth
+                    ;;
+                M)
+                    connect_mode_bluetooth
+                    ;;
+            esac
+        fi
+    done
+}
+
--- test.py
+++
@@ -1,7 +0,0 @@
-def pwm_fancontrol(hysteresis, starttemp, temp):
-    perc = 100.0 * ((temp - (starttemp-hysteresis)) / (starttemp - (starttemp-hysteresis)))
-
-    perc = min(max(perc, 0.0), 100.0)
-    print(float(perc))
-pwm_fancontrol(10,60,40
-               )
