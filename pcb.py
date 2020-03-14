0526f0af2ad17fd44f8b6fd0a0a97132f2b9f9e8
Michael <michael_kirsch>
2020-03-14 15:26:09 +0100

    updated description

--- pcb.py
+++ pcb.py
@@ -5,22 +5,16 @@
 #to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-import http.server
-import configparser
+
 import time
 import os
 import RPi.GPIO as GPIO
-import subprocess
-from configparser import SafeConfigParser
-from enum import Enum
 
 pcb_components={"LED":7,"FAN":8,"RESET":3,"POWER":5,"CHECK_PCB":10}
 
 class path():
     kintaro_folder = "/opt/kintaro/"
     start_folder = "start/"
-    intro_video = kintaro_folder + start_folder + "intro.mp4"
-    config_file = kintaro_folder + start_folder + "kintaro.config"
     temp_command = 'vcgencmd measure_temp'
 
 class vars():
@@ -59,10 +53,6 @@ class led:  #class to control the led
             led.toggle(0)
             time.sleep(interval)
 
-def return_config_bool(searchterm):
-    Config = configparser.ConfigParser()
-    Config.read(path.config_file)  # read the configfile
-    return Config.getboolean("Boot", searchterm)
 
 def fan(status):  #switch the fan on or off
     if status == 1:
@@ -75,20 +65,6 @@ def fancontrol(hysteresis,starttemp):  #read the temp and have a buildin hystere
         fan(1)
     if temp() < starttemp-hysteresis:
         fan(0)
-
-if return_config_bool("video"):
-    os.system("omxplayer " + path.intro_video + " &") #start the bootvideo on start
-
-def toggle(toggle_this):  #change one of the values in the config file
-    parser = configparser.ConfigParser()
-    parser.read(path.config_file)
-    if return_config_bool(toggle_this):
-        parser.set('Boot', toggle_this, "False")
-        fan(0)
-    else:
-        parser.set('Boot', toggle_this, "True")
-    with open(path.config_file, "w+") as configfile:
-        parser.write(configfile)
 
 def Falling_Power(channel):
     time.sleep(0.5)
@@ -128,16 +104,13 @@ GPIO.add_event_detect(pcb_components["CHECK_PCB"],GPIO.RISING,callback=PCB_Pull)
 
 time.sleep(1)
 
-if return_config_bool("pcb") and GPIO.input(pcb_components["CHECK_PCB"])==GPIO.LOW: #check if there is an pcb and if there is then attach the interrupts
+if GPIO.input(pcb_components["CHECK_PCB"])==GPIO.LOW: #check if there is an pcb and if there is then attach the interrupts
     led.toggle(0.5)
     GPIO.add_event_detect(pcb_components["RESET"], GPIO.FALLING, callback=Falling_Reset)
     time.sleep(0.5)
     GPIO.add_event_detect(pcb_components["POWER"], GPIO.FALLING, callback=Falling_Power)
 
-
-    #check for interrupts :D
 while True:
     time.sleep(5)
     led.toggle(1)
-    if return_config_bool("fan"): #check if the fan is activated in the config
-        fancontrol(vars.fan_hysteresis , vars.fan_starttemp) # fan starts at 60 degrees and has a 5 degree hysteresis
+    fancontrol(vars.fan_hysteresis , vars.fan_starttemp) # fan starts at 60 degrees and has a 5 degree hysteresis
